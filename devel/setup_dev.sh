ff66720018c086e7650c80a63cf0d279c1146197
Jan Kubovy <jan@kubovy.eu>
2020-02-09 18:48:04 +0100

    Add packages.

--- devel/setup_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-DIRECTORY=$(dirname $0)
-ln -nfs /share/Private-JanKubovy/Workspace/Footprint/shared/web/action.php /share/CACHEDEV1_DATA/.qpkg/Footprint/web/action.php
-ln -nfs /share/Private-JanKubovy/Workspace/Footprint/shared/web/index.html /share/CACHEDEV1_DATA/.qpkg/Footprint/web/index.html
